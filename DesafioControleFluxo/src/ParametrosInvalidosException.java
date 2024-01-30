1accdaa42fd173efb7772c857028a70a67c45385
Heitor Pavanelli <heitorhernandes@hotmail.com>
2024-01-29 23:47:03 -0300

    first commit

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+
+public class ParametrosInvalidosException extends Exception{
+    public ParametrosInvalidosException() {
+        super("O valor do segundo numero é menor que o valor do primeiro numero.");
+    }
+}
---
+++ DesafioControleFluxo/src/Processo_Seletivo.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Processo_Seletivo {
+    public static void main(String[] args) throws Exception {
+        try {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Digite o primeiro numero:");
+            int parametroUm = sc.nextInt();
+
+            System.out.println("Digite o segundo numero:");
+            int parametroDois = sc.nextInt();
+
+            subtrair(parametroUm, parametroDois);
+
+
+
+
+        } catch (ParametrosInvalidosException e) {
+            System.out.println(e);
+        }
+
+    }
+
+    static void subtrair(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        int subtracao = parametroDois - parametroUm;
+
+        if (parametroDois < parametroUm) {
+            throw new ParametrosInvalidosException();
+        }
+        
+        for (int i = 1; i <= subtracao; i++) {
+            System.out.println("Imprimindo o numero:" + i);
+        }
+    }
+}
